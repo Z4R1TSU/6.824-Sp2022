8a0385eaf7a914b0f788d14f6e1db5c34082e454
xuzhelei <xuzhelei@bytedance.com>
2025-03-16 00:16:04 +0800

    feat: 实现 worker，包含 map 和 reduce 任务的执行逻辑

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,6 +1,13 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strings"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
@@ -19,6 +26,14 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
 // main/mrworker.go calls this function.
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
@@ -27,7 +42,130 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
+	for {
+		task, err := CallAssignTask()
+		if err != nil {
+			log.Fatalf("AssignTask failed: %v", err)
+		}
+		switch task.Task.TaskType {
+		case MapTaskType:
+			doMapTask(mapf, task.Task.TaskId, task.Task.Filename, task.Task.ReduceId)
+		case ReduceTaskType:
+			doReduceTask(reducef, task.Task.TaskId, task.Task.ReduceId)
+		default:
+			log.Fatalf("Unknown task type: %v", task.Task.TaskType)
+		}
+	}
+}
 
+func doMapTask(mapf func(string, string) []KeyValue, taskId int, filename string, nReduce int) {
+	// 1. 读取要被处理的文件内容
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Map: cannot open: %v", filename)
+	}
+	content, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Map: cannot read: %v", filename)
+	}
+	err = file.Close()
+	if err != nil {
+		log.Fatalf("Map: cannot close: %v", filename)
+	}
+	// 2. 执行 mapf 并保存到中间变量（同 key 会被保存在同一中间文件中，因此一组）中
+	kva := mapf(filename, string(content))
+	var intermediate [][]KeyValue
+	// 3. 由键值对中 map 结果的 key 获取中间文件的 Y 值，也就是要处理 reduce 的 worker ID
+	for _, kv := range kva {
+		y := ihash(kv.Key) % nReduce
+		intermediate[y] = append(intermediate[y], kv)
+	}
+	// 4. 将中间结果集变量保存结果当中间文件中
+	for y := 0; y < nReduce; y++ {
+		// 4.1. 文件命名和临时文件创建
+		intermediateFilename := fmt.Sprintf("mr-%d-%d", taskId, y)
+		tempFile, err := os.CreateTemp("./", intermediateFilename)
+		if err != nil {
+			log.Fatalf("Map: cannot create temp file: %v", intermediateFilename)
+		}
+		// 4.2. 将结果写入到临时文件
+		enc := json.NewEncoder(tempFile)
+		for _, kv := range intermediate[y] {
+			err := enc.Encode(kv)
+			if err != nil {
+				log.Fatalf("Map: cannot encode temp file: %v", intermediateFilename)
+			}
+		}
+		// 4.3. 将临时文件重命名成中间文件
+		err = tempFile.Close()
+		if err != nil {
+			log.Fatalf("Map: cannot close temp file: %v", intermediateFilename)
+		}
+		err = os.Rename(tempFile.Name(), intermediateFilename)
+		if err != nil {
+			log.Fatalf("Map: cannot rename temp file: %v", intermediateFilename)
+		}
+	}
+}
+
+func doReduceTask(reducef func(string, []string) string, taskId int, reduceId int) {
+	// 1. 扫描当前路径中的所有文件
+	files, err := os.ReadDir(".")
+	if err != nil {
+		log.Fatalf("Reduce: cannot read dir: %v", err)
+	}
+	// 2. 从中间文件中读取键值对
+	var kva []KeyValue
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		// 2.1. 判断当前目录下的文件是否是本 reduce worker 需要执行的中间文件
+		hasRightPrefix := strings.HasPrefix(file.Name(), "mr-")
+		hasRightSuffix := strings.HasSuffix(file.Name(), fmt.Sprintf("-%d", reduceId))
+		if hasRightPrefix && hasRightSuffix {
+			// 2.2. 若需要读取，则将内容读取到键值对集合变量中
+			file, err := os.Open(file.Name())
+			if err != nil {
+				log.Fatalf("Reduce: cannot open file: %v", err)
+			}
+			dec := json.NewDecoder(file)
+			for {
+				var kv KeyValue
+				if err := dec.Decode(&kv); err != nil {
+					break
+				}
+				kva = append(kva, kv)
+			}
+		}
+	}
+	// 3. 执行 reducef 前的前置初始化
+	sort.Sort(ByKey(kva))
+	filename := fmt.Sprintf("mr-out-%d", reduceId)
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatalf("Reduce: cannot create file: %v", err)
+	}
+	// 4. 执行 mapducef
+	i := 0
+	for i < len(kva) {
+		j := i + 1
+		for j < len(kva) && kva[j].Key == kva[j].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, kva[k].Value)
+		}
+		output := reducef(kva[i].Key, values)
+		// 4.1. 写入结果键值对到最终文件中
+		_, err := fmt.Fprintf(file, "%v %v\n", kva[i].Key, output)
+		if err != nil {
+			log.Fatalf("Reduce: cannot write file: %v", err)
+		}
+		i = j
+	}
+	file.Close()
 }
 
 // example function to show how to make an RPC call to the coordinator.
